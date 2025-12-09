00597666695d1d99583b554f6e2af4ffd2f21265
Dorothea Fennell <31896923+dfennell42@users.noreply.github.com>
2025-12-09 12:40:57 -0500

    Update main.py

--- src/delafossite_wf/main.py
+++ src/delafossite_wf/main.py
@@ -25,6 +25,7 @@ from .pdos_INCARmod import process_pdos_dirs
 #bash script process pdos
 from .vasp_pdos import process_pdos_dirs as parse_pdos_dirs
 from .integrate_pdos import integrate_all_pdos
+from .tot_int import get_all_data
 #plot pdos
 from .PDOS_plotter import plot_pdos
 #initialize
@@ -85,11 +86,13 @@ def parse():
     '''Parses PDOS data into individual files and integrates '''
     parse_pdos_dirs(os.getcwd())
     integrate_all_pdos(os.getcwd())
+    get_all_data(os.getcwd())
     
 @app.command()
 def integrate():
     '''Integrates the PDOS files. Note: Files must be parsed before integration. The parse command parses AND integrates, so this command is only if integration needs to be performed on already parsed files. '''
     integrate_all_pdos(os.getcwd())
+    get_all_data(os.getcwd())
     
 @app.command()
 def plot(
